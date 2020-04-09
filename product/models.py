29a8a0ff69a70ff0b07c2e3924a7c84f558b6232
allen <allen.liu@tpgtelecom.com.au>
2020-04-09 10:34:30 +1000

    first commit

--- product/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class milk(models.Model):
-    产品=models.CharField(max_length = 20)
-    供货价_含运费= models.FloatField()
-    零售价_含运费= models.FloatField()
-    id = models.IntegerField(primary_key = True)
